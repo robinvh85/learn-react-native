1fe68ef46ee7d294ef15fe4711dad3c5c45d1713
VH <vh@Apples-MacBook-Pro.local>
2018-09-11 06:12:27 +0700

    Add project album

---
+++ albums/components/header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = () => {
+  return (
+    <View style={stylesheet.viewStyle}>
+      <Text style={stylesheet.textStyle}>Albumn</Text>
+    </View>
+  );
+}
+
+const stylesheet = {
+  viewStyle: {
+    backgroundColor: '#FAFAFA',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2
+  },
+  textStyle: {
+    fontSize: 30,
+    color: 'red'
+  }
+}
+
+export default Header;
