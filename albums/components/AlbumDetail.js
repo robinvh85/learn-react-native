d2d0eea5d11c58cd4703de8402d9b4e3751710da
VH <vh@Apples-MacBook-Pro.local>
2018-09-12 06:11:50 +0700

    Style for area

--- albums/components/AlbumDetail.js
+++ albums/components/AlbumDetail.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import Card from './Card'
+import CardSection from './CardSection'
 
 const AlbumDetail = (props) => {
   return (
-    <View>
-      <Text>{ props.album.title }</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <View>
+          <Image style={styles.thumbnailStyle} source={{ uri: props.album.thumbnail_image }} />
+        </View>
+        <View style={styles.headerContainerStyle}>
+          <Text>{ props.album.title }</Text>
+          <Text>{ props.album.artist }</Text>
+        </View>
+      </CardSection>
+    </Card>
   )
 };
+
+const styles = {
+  headerContainerStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
+}
 
 export default AlbumDetail;
